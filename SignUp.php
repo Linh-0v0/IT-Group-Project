dcde802808427c7f70b57b7ee7d5ce41f9504bee
Linh-0v0 <s3864120@rmit.edu.vn>
2021-01-12 20:41:42 +0700

    testing php

--- SignUp.php
+++ SignUp.php
@@ -1,6 +1,10 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
-?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Developer News</h1>
+
+<?php echo "The Best PHP Examples"; ?>
+
+</body>
+</html> 
