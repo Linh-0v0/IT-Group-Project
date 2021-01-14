c40d97e207a81767b77df03d01bd3f4682e75e62
Linh-0v0 <s3864120@rmit.edu.vn>
2021-01-15 00:10:01 +0700

    fixing

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2fd42a2c4c18295afa0772d559a2ff1755777c18',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2fd42a2c4c18295afa0772d559a2ff1755777c18',
+    ),
+  ),
+);
