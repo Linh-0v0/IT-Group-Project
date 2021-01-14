2fd42a2c4c18295afa0772d559a2ff1755777c18
Linh-0v0 <s3864120@rmit.edu.vn>
2021-01-14 23:48:38 +0700

    add file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '413bffe9e4b64a74ad90c6da129fc51f663cfa7e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '413bffe9e4b64a74ad90c6da129fc51f663cfa7e',
+    ),
+  ),
+);
