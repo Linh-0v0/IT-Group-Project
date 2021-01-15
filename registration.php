937ea93508fe252b89dd662b4f9f08be7b97e296
Linh-0v0 <s3864120@rmit.edu.vn>
2021-01-15 11:02:03 +0700

    change to herokuDB, lack in code

--- registration.php
+++ registration.php
@@ -3,15 +3,14 @@
 session_start();
 header('location:index.php');
 
-$con = mysqli_connect('remotemysql.com:3306','jZfcTdzic9','P1ErebOs6Q');
+$con = mysqli_connect('eu-cdbr-west-03.cleardb.net','bb3ba2919b47ef','fb8de583');
 
-mysqli_select_db($con, 'jZfcTdzic9');
-
+mysqli_select_db($con, 'heroku_6a1a16c55a7c7d8');
 
 $name = $_POST['user'];
 $pass = $_POST['password'];
 
-$s = " select * from usertable where name = '$name'";
+$s = " select * from usertable where name = '$name' && password = '$pass'";
 
 $result = mysqli_query($con, $s);
 
--- validation.php
+++ validation.php
@@ -2,9 +2,9 @@
 
 session_start();
 
-$con = mysqli_connect('remotemysql.com:3306','jZfcTdzic9','P1ErebOs6Q');
+$con = mysqli_connect('eu-cdbr-west-03.cleardb.net','bb3ba2919b47ef','fb8de583');
 
-mysqli_select_db($con, 'jZfcTdzic9');
+mysqli_select_db($con, 'heroku_6a1a16c55a7c7d8');
 
 $name = $_POST['user'];
 $pass = $_POST['password'];
